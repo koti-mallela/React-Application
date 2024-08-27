b1539dc965648b8aabf7cd2192568fef762c27fb
Koteswara Rao Mallela <kotimallela0415@gmail.com>
2024-08-27 17:58:31 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import KeyDemo from "./components/KeyDemo"
+import Stopwatch from "./components/Stopwatch"
+import ToDoList from "./components/ToDoList"
+
+function App() {
+  return(
+    <>
+        <KeyDemo/>
+        <ToDoList/>
+        <Stopwatch/>
+    </>
+  )
+  }
+
+export default App
---
+++ main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import './ToDoList.css'
+import './Stopwatch.css'
+import  '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/bootstrap-icons/font/bootstrap-icons.css'
+import App from './App'
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App/>
+  </React.StrictMode>,
+)
